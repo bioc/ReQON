bc8afbb42b5267d62ee07a7f92d07aeb8af04cfa
c.cabanski <unknown>
2012-07-02 17:57:52 +0000

    fixed output bam bug
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ReQON@67208 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ReadPosErrorPlot.R
+++ R/ReadPosErrorPlot.R
@@ -1,8 +1,8 @@
-ReadPosErrorPlot <- function(ReadPosErrors, error_col = "blue", thresh = 1.5, thresh_col = "cyan",
+ReadPosErrorPlot <- function(ReadPosErrors, startpos = 1, error_col = "blue", thresh = 1.5, thresh_col = "cyan",
    xlabel = "Read Position", ylabel = "# Errors", main_title = "Distribution of Errors by Read Position"){
    
     readlen <- length(ReadPosErrors)
-    plot(c(1:readlen), ReadPosErrors, 'l', col = error_col, xlab = xlabel, ylab = ylabel) 
+    plot(c(startpos:(readlen-1+startpos)), ReadPosErrors, 'l', col = error_col, xlab = xlabel, ylab = ylabel) 
 	title(main = list(main_title, cex=1))   
     lines(c(1:readlen), matrix(1,1,readlen)*thresh*(sum(ReadPosErrors)/readlen), col = thresh_col,
 	   lwd = 2, lty = 2)
